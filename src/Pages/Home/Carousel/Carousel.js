0deb7012c42c0d8c97da663942207c4e9600066c
Rejaur9131 <raju_seu@yahoo.com>
2022-05-08 00:19:16 +0600

    all img loading problem solved for live server

--- src/Pages/Home/Carousel/Carousel.js
+++ src/Pages/Home/Carousel/Carousel.js
@@ -10,10 +10,10 @@ const Carousel = () => {
       </div>
       <div className="carousel-inner relative w-full overflow-hidden">
         <div className="carousel-item active float-left w-full ">
-          <img src="http://htmldemo.magikthemes.com/ecommerce/brezza-html-template/furniture/images/slide-img1.jpg" className="block w-full" alt="Wild Landscape" />
+          <img src="https://i.ibb.co/gR2n7kG/slide-img1.jpg" className="block w-full" alt="" />
         </div>
         <div className="carousel-item float-left w-full">
-          <img src="http://htmldemo.magikthemes.com/ecommerce/brezza-html-template/furniture/images/slide-img2.jpg" className="block w-full" alt="Camera" />
+          <img src="https://i.ibb.co/1TtM00H/slide-img2.jpg" className="block w-full" alt="" />
         </div>
       </div>
       <button className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
